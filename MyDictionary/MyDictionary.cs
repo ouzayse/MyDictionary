762c14ce3bb1740dfb86ef0d1ebcfc8bbfdc28c9
Ayşe Oğuz <ouzayse123@gmail.com>
2021-01-23 19:51:39 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TValue, TKey>
+    {
+        TValue[] values;
+        TKey[] keys;
+        public MyDictionary() 
+        {
+            values = new TValue[0];
+            keys = new TKey[0];
+        }
+        public void Add(TValue value, TKey key)
+        {
+            TValue[] tempValues = values;
+            TKey[] tempKeys = keys;
+
+            values = new TValue[values.Length + 1];
+            keys = new TKey[keys.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                values[i] = tempValues[i];
+                keys[i] = tempKeys[i];
+            }
+            values[values.Length - 1] = value;
+            keys[keys.Length - 1] = key;
+        }
+        public void GetProducts()
+        {
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.WriteLine(values[i] + " : " + keys[i] + "  ürünü sisteme eklendi.");
+            }
+
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> products = new MyDictionary<int, string>();
+
+            products.Add(1, "Kalem");
+            products.Add(2, "Kulaklık");
+            products.Add(3, "Masa");
+            products.Add(4, "Saat");
+
+            products.GetProducts();
+        }
+    }
+}
